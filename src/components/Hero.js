69e208805e39f9bfba02725e7c23ccdff6be9894
Carlos <cjarquin1420@gmail.com>
2022-08-02 11:17:44 +0800

    First Commit

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default function Hero() {
+    return(
+        <section className="hero-section">
+            <img
+                src="/images/photo-grid.png"
+                alt="image"
+                className= "photo-grid" />
+            <h1 className="hero-header"> Online Experiences</h1>
+            <p className="hero-text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img
+                className= "airbnb-logo"
+                src="/images/airbnb-logo.png"
+            ></img>
+        </nav>
+    )
+}
